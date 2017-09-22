5ea8f588c3486d3b64710475bff0c1950be20ba3
Marek Kirejczyk <marekkirejczyk@gmail.com>
2017-09-22 12:15:45 +0200

    update constants in contract

--- contracts/AmbrosusSale.sol
+++ contracts/AmbrosusSale.sol
@@ -486,7 +486,7 @@ contract AmbrosusSale {
 	//////
 
 	// How much is enough?
-	uint public constant MIN_BUYIN_VALUE = 1;
+	uint public constant MIN_BUYIN_VALUE = 10000000000000000;
 	// Max gas price for buyins.
 	uint public constant MAX_BUYIN_GAS_PRICE = 25000000000;
 	// The exposed hard cap.
@@ -500,7 +500,7 @@ contract AmbrosusSale {
 	uint constant public LIQUID_ALLOCATION_PPM = 263000;
 
 	/// The certifier resource. TODO: set address
-	Certifier public constant CERTIFIER = Certifier(0x1e2F058C43ac8965938F6e9CA286685A3E63F24E);
+	Certifier public constant CERTIFIER = Certifier(0x7b1Ab331546F021A40bd4D09fFb802261CaACcc9);
 	// Who can halt/unhalt/kill?
 	address public constant ADMINISTRATOR = 0x11bF17B890a80080A8F9C1673D2951296a6F3D91;
 	// Who can prepurchase?
@@ -508,7 +508,7 @@ contract AmbrosusSale {
 	// Who gets the stash? Should not release funds during minting process.
 	address public constant TREASURY = 0xB47aD434C6e401473F1d3442001Ac69cda1dcFDd;
 	// When does the contribution period begin?
-	uint public constant BEGIN_TIME = 1506168000;
+	uint public constant BEGIN_TIME = 1506081600;
 	// How long does the sale last for?
 	uint public constant DURATION = 30 days;
 	// When does the period end?
